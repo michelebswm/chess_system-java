e8f3337b9a443985f09eae0ea64ff8fba75d8484
michelebswm <michele.fenix.17@gmail.com>
2024-01-15 23:05:32 -0300

    feat: jogada especial promocao pawn

--- src/application/Program.java
+++ src/application/Program.java
@@ -40,6 +40,12 @@ public class Program {
                 if (capturedPiece != null){
                     captured.add(capturedPiece);
                 }
+
+                if (chessMatch.getPromoted() != null){
+                    System.out.print("Entre com a peça para promoção (B/N/R/Q): ");
+                    String type = sc.nextLine();
+                    chessMatch.replacePromotedPiece(type);
+                }
             }
             catch (ChessException e){
                 System.out.println(e.getMessage());
